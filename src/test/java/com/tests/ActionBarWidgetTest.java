f52abadec278899c45487c0c7ccb7a517e9d5f2c
Jagan <pylajaganmohan123@gmail.com>
2025-08-09 14:40:16 +0530

    updated testng.xml

--- src/test/java/com/tests/ActionBarWidgetTest.java
+++ src/test/java/com/tests/ActionBarWidgetTest.java
@@ -1,14 +1,8 @@
 package com.tests;
 
-import org.testng.annotations.Test;
-
 import com.base.BaseTest;
 
 public class ActionBarWidgetTest extends BaseTest{
 
-	@Test
-	public void validateBackGroundColor() {
-		actionBarPage = homePage.openActionBar();
-		System.out.println(actionBarPage.getColor());
-	}
+	
 }
